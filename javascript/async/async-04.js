263d2af63926a6a690559359b55963349bf90227
9diin <weartstudio@naver.com>
2024-11-07 10:55:14 +0900

    feat: 자바스크립트 기초

--- javascript/async/async-04.js
+++ javascript/async/async-04.js
@@ -44,4 +44,28 @@ const newDelayAdd = (idx, cb, err) => {
 /** Promise 인스턴스를 반환하기 때문에 then 메서드를 사용할 수 있겠죠? */
 newDelayAdd(9)
     .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+    .catch((err) => console.error(err))
+    /** finally 메서드나 구문 같은 경우에는
+     * 비동기 코드 내부의 resolve, reject이 실행되는 것과 상관없이
+     * 항상 실행되는 구조입니다.
+     */
+    .finally(() => console.log("완료"));
+
+const wrap = async () => {
+    // const res = await newDelayAdd(5);
+    // console.log(res);
+
+    try {
+        const res = await newDelayAdd(13);
+        console.log(res);
+    } catch (err) {
+        console.error(err);
+    } finally {
+        /** finally 메서드나 구문 같은 경우에는
+         * 비동기 코드 내부의 resolve, reject이 실행되는 것과 상관없이
+         * 항상 실행되는 구조입니다.
+         */
+        console.log("완료!");
+    }
+};
+wrap();
