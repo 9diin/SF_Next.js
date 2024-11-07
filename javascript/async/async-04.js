4e89a47ab9a233aaa353c7059b8988bfdda1b60d
9diin <weartstudio@naver.com>
2024-11-07 10:43:07 +0900

    feat: 자바스크립트 기초

---
+++ javascript/async/async-04.js
@@ -0,0 +1,47 @@
+/** resolve, reject 그리고 에러 핸들링 */
+
+const delayAdd = (index, callback, error) => {
+    setTimeout(() => {
+        if (index > 10) {
+            error(`${index}는 10보다 클 수 없습니다.`);
+            return;
+        }
+        console.log("index:", index);
+        callback(index + 1);
+    }, 1000);
+};
+
+delayAdd(
+    4,
+    (res) => console.log(res),
+    (err) => console.error(err)
+);
+
+delayAdd(
+    13,
+    (res) => console.log(res),
+    (err) => console.error(err)
+);
+
+/** 에러 핸들링
+ * 정상적으로 로직이 동작하게 되면, 두 번째 인수 부분의 콜백이 실행되는 것이고
+ * 정상적으로 로직이 동작하게 되지 않게 되면, 세 번째 인수 부분의 콜백이 실행되는 구조
+ */
+
+const newDelayAdd = (idx, cb, err) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (idx > 10) {
+                reject(`${idx}는 10보다 클 수 없습니다.`);
+                return;
+            }
+            console.log("index:", idx);
+            resolve(idx + 1);
+        }, 1000);
+    });
+};
+
+/** Promise 인스턴스를 반환하기 때문에 then 메서드를 사용할 수 있겠죠? */
+newDelayAdd(9)
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
