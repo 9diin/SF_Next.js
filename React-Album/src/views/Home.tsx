a07cc4ecfa3744f63eeb60e02587b9f8259fd5ac
9Diin <weartstudio@naver.com>
2024-11-15 13:51:51 +0900

    feat: 헤더 및 네비게이션 UI 개발 중 with json 파일로 네비게이션 메뉴 관리하기

--- React-Album/src/views/Home.tsx
+++ React-Album/src/views/Home.tsx
@@ -1,10 +1,11 @@
-import { Header } from "@/components/common";
+import { Header, Nav } from "@/components/common";
 
 function HomePage() {
     return (
         <div className="page">
             <div className="page__container">
                 <Header />
+                <Nav />
             </div>
         </div>
     );
