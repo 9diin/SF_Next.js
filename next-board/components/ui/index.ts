40424f840a5daeb8579ee0705648342e25ec0b86
9Diin <weartstudio@naver.com>
2024-11-19 17:28:22 +0900

    chore: 다이얼로그 컴포넌트 추가 및 코드 입히기

--- next-board/components/ui/index.ts
+++ next-board/components/ui/index.ts
@@ -2,6 +2,18 @@ export { Button, buttonVariants } from "./button/button";
 export { Calendar } from "./calendar/calendar";
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card/card";
 export { Checkbox } from "./checkbox/checkbox";
+export {
+    Dialog,
+    DialogPortal,
+    DialogOverlay,
+    DialogClose,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+} from "./dialog/dialog";
 export { BasicDatePicker } from "./date-picker/date-picker";
 export { LabelDatePicker } from "./date-picker/label-date-picker";
 export { Input } from "./input/input";
