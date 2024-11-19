7b70a744e09fa2e6b5fcd9beb4fe647c7829ec59
9Diin <weartstudio@naver.com>
2024-11-19 14:26:19 +0900

    feat: 게시물 조회 페이지 HEADER 헤더 Ui 개발

--- next-board/app/board/[id]/page.tsx
+++ next-board/app/board/[id]/page.tsx
@@ -1,7 +1,15 @@
-import { Button, Input, SearchBar, Progress } from "@/components/ui";
+"use client";
+
+import { useState } from "react";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+import { Button, Calendar, SearchBar, Progress, Popover, PopoverContent, PopoverTrigger } from "@/components/ui";
+import { Calendar as CalendarIcon } from "lucide-react";
 import styles from "./page.module.scss";
 
 function BoardPage() {
+    const [date, setDate] = useState<Date>();
+
     return (
         <div className="page">
             <aside className="page__aside">
@@ -28,16 +36,63 @@ function BoardPage() {
             </aside>
             <main className="page__main">
                 <div className={styles.header}>
-                    <div className={styles.heaer__top}>
+                    <div className={styles.header__top}>
                         {/* 제목 입력 Input 섹션 */}
-                        <Input placeholder="Enter Title Here!" />
+                        <input type="text" placeholder="Enter Title Here!" className={styles.header__top__input} />
                         {/* 진행상황 척도 그래프 섹션 */}
                         <div className="flex items-center justify-start gap-4">
                             <small className="text-sm font-medium leading-none text-[#6D6D6D]">1/10 Completed</small>
                             <Progress className="w-60 h-[10px]" />
                         </div>
                     </div>
-                    <div>{/* 캘린더 + Add New Board 버튼 섹션 */}</div>
+                    {/* 캘린더 + Add New Board 버튼 섹션 */}
+                    <div className={styles.header__bottom}>
+                        <div className="flex items-center">
+                            <div className="flex items-center gap-3">
+                                <small className="text-sm font-medium leading-none text-[#6D6D6D]">From </small>
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Button
+                                            variant={"outline"}
+                                            className={cn(
+                                                "w-[280px] justify-start text-left font-normal",
+                                                !date && "text-muted-foreground"
+                                            )}
+                                        >
+                                            <CalendarIcon className="mr-2 h-4 w-4" />
+                                            {date ? format(date, "PPP") : <span>Pick a date</span>}
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0">
+                                        <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+                                    </PopoverContent>
+                                </Popover>
+                            </div>
+                            <div className="flex items-center gap-3">
+                                <small className="text-sm font-medium leading-none text-[#6D6D6D]">To</small>
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Button
+                                            variant={"outline"}
+                                            className={cn(
+                                                "w-[280px] justify-start text-left font-normal",
+                                                !date && "text-muted-foreground"
+                                            )}
+                                        >
+                                            <CalendarIcon className="mr-2 h-4 w-4" />
+                                            {date ? format(date, "PPP") : <span>Pick a date</span>}
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0">
+                                        <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+                                    </PopoverContent>
+                                </Popover>
+                            </div>
+                        </div>
+                        <Button className="text-white bg-[#E79057] hover:bg-[#E79057] hover:border hover:border-[#E26F24]">
+                            Add New Board
+                        </Button>
+                    </div>
                 </div>
                 <div className={styles.body}></div>
             </main>
